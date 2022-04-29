fe8e27721ee2680a783a36e3e60561021b87f7c0
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-04-29 12:23:55 +0100

    added endpoint & method for authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(AuthController::class)->group(function() {
+
+    Route::prefix('/auth')->group(function() {
+
+        Route::post('/login', 'login')->name('login');
+
+        Route::post('/register', 'register')->name('register');
+
+    });
+
 });
