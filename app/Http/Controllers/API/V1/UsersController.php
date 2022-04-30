8831b40ad0a67472b7b098e88bdb60effe339551
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-04-30 12:45:30 +0100

    added controller

---
+++ app/Http/Controllers/API/V1/UsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use Illuminate\Http\Request;
+use App\Repositories\UserInterface;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
+
+class UsersController extends Controller
+{
+    protected $users_repo;
+
+    public function __construct(UserInterface $user_contract)
+    {
+        $this->users_repo = $user_contract;
+    }
+
+    public function store(CreateUserRequest $request)
+    {
+        # code ...
+    }
+
+}
