f8ef0e4c851222cd1a02076c36d6d05dae7c21f9
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-04-30 10:14:38 +0100

    added admin login method & removed register endpoint

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -2,19 +2,38 @@
 
 namespace App\Http\Controllers\API\V1;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+
     public function login(LoginRequest $request)
     {
+        $validated = $request->safe()->only(['email', 'password']);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || ! Hash::check($validated['password'], $user->password)) {
+            return response()->json([
+                "status" => Response::HTTP_BAD_REQUEST,
+                "message" => "The provided credentials are incorrect.",
+                "data" => null
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user->token = $user->createToken('glover')->plainTextToken;
+
+        return response()->json([
+            'status' => Response::HTTP_OK,
+            'message' => 'Login is successful',
+            'data' => $user
+        ], Response::HTTP_OK);
 
     }
 
-    public function register()
-    {
-
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,6 @@ Route::controller(AuthController::class)->group(function() {
 
         Route::post('/login', 'login')->name('login');
 
-        Route::post('/register', 'register')->name('register');
-
     });
 
 });
