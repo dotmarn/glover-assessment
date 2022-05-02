872249e2f2b7776b1c49f33d511ffb5dd317fd6c
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-05-02 15:00:21 +0100

    changes

--- database/migrations/2022_04_30_095044_create_user_requests_table.php
+++ database/migrations/2022_04_30_095044_create_user_requests_table.php
@@ -15,23 +15,13 @@ return new class extends Migration
     {
         Schema::create('user_requests', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('admin_id');
+            $table->integer('admin_id');
             $table->integer('user_id')->nullable();
             $table->enum('request_type',['create', 'update', 'delete']);
             $table->json('payload')->nullable();
             $table->enum('status',['pending', 'approved', 'declined'])->default('pending');
-            $table->unsignedBigInteger('approved_by')->nullable();
+            $table->integer('approved_by')->nullable();
             $table->timestamps();
-
-            $table->foreign('admin_id')
-                    ->references('id')
-                    ->on('users')
-                    ->onDelete('cascade');
-
-            $table->foreign('approved_by')
-                    ->references('id')
-                    ->on('users')
-                    ->onDelete('cascade');
         });
     }
 
